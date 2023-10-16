c7e138d5a0a8fecfaf39d680f716fb8438545d0a
Nekotato1412 <nekotato1412@gmail.com>
2023-10-16 15:44:25 -0500

    Refined Scaling

--- components/dialog_box/DialogBox.gd
+++ components/dialog_box/DialogBox.gd
@@ -3,26 +3,32 @@ extends Node
 func _on_Main_presentation_mode():
 	$DialogContent._on_Main_presentation_mode()
 	$LineEdit._on_Main_presentation_mode()
-	$D_Scale_Widget_X._on_Main_presentation_mode()
-	$D_Scale_Widget_Y._on_Main_presentation_mode()
-	$N_Scale_Widget_X._on_Main_presentation_mode()
-	$N_Scale_Widget_Y._on_Main_presentation_mode()
+	
+	for widget in $D_Widgets.get_children():
+		widget._on_Main_presentation_mode()
+	
+	for widget in $N_Widgets.get_children():
+		widget._on_Main_presentation_mode()
 
 func _on_Main_edit_mode():
 	$DialogContent._on_Main_edit_mode()
 	$LineEdit._on_Main_edit_mode()
-	$D_Scale_Widget_X._on_Main_edit_mode()
-	$D_Scale_Widget_Y._on_Main_edit_mode()
-	$N_Scale_Widget_X._on_Main_edit_mode()
-	$N_Scale_Widget_Y._on_Main_edit_mode()
+	
+	for widget in $D_Widgets.get_children():
+		widget._on_Main_edit_mode()
+		
+	for widget in $N_Widgets.get_children():
+		widget._on_Main_edit_mode()
 
 func _on_Main_setup_mode():
 	$DialogContent._on_Main_setup_mode()
 	$LineEdit._on_Main_setup_mode()
-	$D_Scale_Widget_X._on_Main_setup_mode()
-	$D_Scale_Widget_Y._on_Main_setup_mode()
-	$N_Scale_Widget_X._on_Main_setup_mode()
-	$N_Scale_Widget_Y._on_Main_setup_mode()
+	
+	for widget in $D_Widgets.get_children():
+		widget._on_Main_setup_mode()
+	
+	for widget in $N_Widgets.get_children():
+		widget._on_Main_setup_mode()
 
 func _on_Main_font_change(path):
 	$DialogContent._on_Main_font_change(path)
